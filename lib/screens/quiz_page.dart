8ddde54e4338de54bbac8b07c5ecdd9650cb2883
SILVERGOLDZ <silvergoldzgame@gmail.com>
2025-10-31 16:38:55 +0700

    feat<form>: Created name input on HomePage

--- lib/screens/quiz_page.dart
+++ lib/screens/quiz_page.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
+import '/provider/user_provider.dart';
+import 'package:provider/provider.dart';
 
-
-class QuizPage extends StatefulWidget{
+class QuizPage extends StatelessWidget{
   const QuizPage({super.key});
 
   @override
-  State<QuizPage> createState() =>  _HomePageState();
-}
-
-class  _HomePageState extends State<QuizPage> {
-
-  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Center(
-        child: Text('ini Quiz Page'),
-      ),
+      // appBar: AppBar(title: const Text('Persistent Form')),
+      // body: Padding(
+      //   padding: const EdgeInsets.all(16.0),
+      //   child: Column(
+      //     children: [
+      //       TextField(
+      //         decoration: const InputDecoration(labelText: 'Name'),
+      //         onChanged: (value) => formData.updateName(value),
+      //         controller: TextEditingController(text: formData.name),
+      //       ),
+      //       TextField(
+      //         decoration: const InputDecoration(labelText: 'Email'),
+      //         onChanged: (value) => formData.updateEmail(value),
+      //         controller: TextEditingController(text: formData.email),
+      //       ),
+      //       const SizedBox(height: 20),
+      //       Text('Current Name: ${formData.name}'),
+      //       Text('Current Email: ${formData.email}'),
+      //     ],
+      //   ),
+      // ),
     );
-    throw UnimplementedError();
   }
 }
