da328b2437491d3ffb338881adbf339cc43f5cbb
Steven Girsang <167726244+SILVERGOLDZ@users.noreply.github.com>
2025-11-04 14:18:30 +0700

    feat<Quiz>: Created reusable Card Widget and applied at QuizPage (#4)
    Added:
    
    1. Reusable Card Widget for Quiz
    2. Create QuizPage with applied Card Widget
    3. Submit button
    
    Still on prototype, not functional

---
+++ lib/data/dummy.dart
@@ -0,0 +1,22 @@
+import '/model/quiz_list.dart';
+
+class DummyData{
+  static final List<QuizList> pertanyaan = [
+    const QuizList(
+        soal: '\ttestvery LONGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG TEXT',
+        opsi: ["A", "B", "C", "D"]
+    ),
+    const QuizList(
+        soal: '\ttest2',
+        opsi: ["A", "B", "C", "D"]
+    ),
+    const QuizList(
+        soal: '\ttest3',
+        opsi: ["A", "B", "C", "D"]
+    ),
+    const QuizList(
+        soal: '\ttes4',
+        opsi: ["A", "B", "C", "D"]
+    ),
+  ];
+}
